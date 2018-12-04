0615bba4832dbeaeb2f55dcc63ef4fb5347f9993
Toby Kurien <toby@tobykurien.com>
2018-12-04 19:56:09 +0200

    fix nrf pins

--- glider_tx/src/glider_tx.ino
+++ glider_tx/src/glider_tx.ino
@@ -25,12 +25,14 @@
   #define LEFT_RIGHT  P1_1
   #define UP_DOWN     P1_3
   #define BATTERY     P1_4
+  Enrf24 radio(P2_0, P2_1, P2_2); // P2.0=CE, P2.1=CSN, P2.2=IRQ
 #else
   #define TRIM_BUTTON P2_1
   #define BUZZER      P2_0 
   #define LEFT_RIGHT  P1_0
   #define UP_DOWN     P1_3
   #define BATTERY     P1_4
+  Enrf24 radio(P2_6, P2_7, P2_5); // CE, CSN, IRQ
 #endif
 
 // Hard-coded parameters
@@ -39,7 +41,6 @@
 #define MAX_TRIM        350 // max allowed trim 0 -> 512
 #define TRIM_TIMEOUT    2000  // min millis between trims
 
-Enrf24 radio(P2_0, P2_1, P2_2); // P2.0=CE, P2.1=CSN, P2.2=IRQ
 const uint8_t rxaddr[] = {0xDE, 0xAD, 0xBE, 0xEF, 0x01};
 unsigned char joystick[2];
 
