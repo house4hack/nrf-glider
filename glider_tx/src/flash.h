76ec7511cb397085f92c4efbf8ea5ef956bbd3a9
Toby Kurien <toby@tobykurien.com>
2019-01-08 15:14:00 +0200

    update flags

--- glider_tx/src/flash.h
+++ glider_tx/src/flash.h
@@ -1,19 +1,16 @@
 #ifndef __FLASH_H__
 #define __FLASH_H__
 #include "MspFlash.h"
-#include "glider_tx.h"
 #include "config.h"
 
 #define flash        SEGMENT_D // four segments, segment A read-only
 #define CHECK_BYTE  0xA1  // indicates if flash has valid data
 
 // What is stored in the flags byte
-enum Flags {
-  InvertUpDown,
-  InvertLeftRight,
-  SwapAxes,
-  EnableMixing
-};
+#define InvertUpDown      0b00000001
+#define InvertLeftRight   0b00000010
+#define SwapAxes          0b00000100
+#define EnableMixing      0b00001000
 
 // Config data stored into flash (max 64 bytes)
 struct FlashData {
